d2a0a69fb3f8d96a012555fb6ce969a4f8f3165f
Matheus Rosendo <matheus.rosendo@my-oli.com>
2026-02-04 17:55:30 -0300

    refactor: removed OcnClientConfig

--- src/main/java/com/banula/tariffmanager/TMBackendApplication.java
+++ src/main/java/com/banula/tariffmanager/TMBackendApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.banula.openlib", "com.olisystems.privatelib", "com.banula" })
+@ComponentScan(basePackages = { "com.banula.openlib", "com.olisystems.privatelib",
+		"com.banula.tariffmanager" })
 public class TMBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TMBackendApplication.class, args);
